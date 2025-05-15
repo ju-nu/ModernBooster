0b8ec8ae8b817934fe21c70c82de103a1e43ef77
Sebastian Gräbner <sebastian@ju.nu>
2025-05-15 12:11:44 +0300

    Transition to Theme

--- src/JunuModernBooster.php
+++ src/JunuModernBooster.php
@@ -3,7 +3,12 @@
 namespace Junu\ModernBooster;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
 
-class JunuModernBooster extends Plugin
+class JunuModernBooster extends Plugin implements ThemeInterface
 {
+    public function getThemeConfigPath(): string
+    {
+        return 'theme.json';
+    }
 }
